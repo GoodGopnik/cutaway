9e65c111dbe96f101a70d2d97f7836781b67da03
GoodGopnik <https://github.com/GoodGopnik>
2021-05-04 21:55:51 +0300

    аботаю над первым блоком

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
-$(function(){
+let btn = document.querySelector('.block__home__menu__btn');
+let blockHomeMenu = document.querySelector('.block__home__menu')
 
-
-
-  
-});
+btn.addEventListener('click', () =>{
+    blockHomeMenu.classList.toggle('display__block__nav')
+})
