0ea973531aebef9618b7394ac3d025bc29290035
GoodGopnik <https://github.com/GoodGopnik>
2021-05-10 11:54:59 +0300

    пробная версия бергера

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,6 @@ let btn = document.querySelector('.block__home__menu__btn');
 let blockHomeMenu = document.querySelector('.block__home__menu')
 
 btn.addEventListener('click', () =>{
-    blockHomeMenu.classList.toggle('display__block__nav')
+    blockHomeMenu.classList.toggle('display__block__nav');
+    blockHomeMenu.classList.toggle('block__home__menu__right');
 })
