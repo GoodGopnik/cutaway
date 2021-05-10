6f243bde00da375bec1d8e8883282fa6b5f3a127
GoodGopnik <https://github.com/GoodGopnik>
2021-05-10 20:57:19 +0300

    аботаю над вторым блоком и менюхой

--- app/js/main.js
+++ app/js/main.js
@@ -3,12 +3,16 @@ let blockHomeMenu = document.querySelector('.block__home__menu');
 let burgerBlock1 = document.querySelector('.burger__block1');
 let burgerBlock2 = document.querySelector('.burger__block2');
 let burgerBlock3 = document.querySelector('.burger__block3');
+let service = document.querySelector('#boxxxx');
+
 
 
 btn.addEventListener('click', () => {
     blockHomeMenu.classList.toggle('display__block__nav');
-    blockHomeMenu.classList.toggle('block__home__menu__right');
+    // blockHomeMenu.classList.toggle('block__home__menu__right');
     burgerBlock1.classList.toggle('burger__block1__active');
     burgerBlock2.classList.toggle('burger__block2__active');
     burgerBlock3.classList.toggle('burger__block3__active');
 })
+
+
