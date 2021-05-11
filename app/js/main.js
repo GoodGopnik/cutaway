86d7f84514a1652dcd590e748e1c252e28043081
GoodGopnik <https://github.com/GoodGopnik>
2021-05-11 18:43:59 +0300

    сделал бургер

--- app/js/main.js
+++ app/js/main.js
@@ -3,16 +3,20 @@ let blockHomeMenu = document.querySelector('.block__home__menu');
 let burgerBlock1 = document.querySelector('.burger__block1');
 let burgerBlock2 = document.querySelector('.burger__block2');
 let burgerBlock3 = document.querySelector('.burger__block3');
-let service = document.querySelector('#boxxxx');
+let serviceBloks = document.querySelector('#service__bloks');
 
 
 
 btn.addEventListener('click', () => {
     blockHomeMenu.classList.toggle('display__block__nav');
-    // blockHomeMenu.classList.toggle('block__home__menu__right');
     burgerBlock1.classList.toggle('burger__block1__active');
     burgerBlock2.classList.toggle('burger__block2__active');
     burgerBlock3.classList.toggle('burger__block3__active');
 })
 
-
+serviceBloks.addEventListener('click', () => {
+    blockHomeMenu.classList.toggle('display__block__nav');
+    burgerBlock1.classList.toggle('burger__block1__active');
+    burgerBlock2.classList.toggle('burger__block2__active');
+    burgerBlock3.classList.toggle('burger__block3__active');
+})
